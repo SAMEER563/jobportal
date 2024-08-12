e32efc45e852615f9d639964e3f4346923f8b03e
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-08-12 10:07:46 +0530

    Schema done

---
+++ backend/models/company.model.js
@@ -0,0 +1,32 @@
+import e from "express";
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    website: {
+        type: String,
+       
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        require: true
+    },
+   
+    location: {
+        type: String,
+        
+    },
+    logo: {
+        type: String,
+        default: ""
+    },
+    description: {
+        type: String,
+        
+    }
+},{timestamps:true});
+export const Company = mongoose.model('Company', companySchema);
---
+++ backend/models/job.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    requirement: {
+        type: String,
+        require: true
+    },
+    location: {
+        type: String,
+        require: true
+    },
+    salary: {
+        type: Number,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    jobType: {
+        type: String,
+        require: true
+    },
+    position: {
+        type: Number,
+        default: Date.now
+    },
+    company: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Company',
+        require: true
+    },
+    created_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        require: true
+    },
+    applications: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Application'
+        }
+    ],
+},{timestamps:true});
+
+export const Job = mongoose.model('Job', jobSchema);
