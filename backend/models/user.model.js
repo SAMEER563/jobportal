02a59c818cac7a801c4b5f8d9862cefd22778b3e
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-08-12 09:44:11 +0530

    First Commit

---
+++ backend/models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    phone: {
+        type: Number,
+        require: true
+    },
+    role: {
+        type: String,
+        enum:['student','recruiter'],
+        require: true
+    },
+    profile: {
+        bio:{type:String},
+        skills:{type:String},
+        resume:{type:String}, // Url to resume
+        resumeOriginalName:{type: String},
+        company:{type:mongoose.Schema.Types.ObjectId, ref:'Company'},
+        profilePhoto:{
+            type:String,
+            default:""
+        }
+    },
+},{timestamps:true});
+ export const User = mongoose.model('User', userSchema);
