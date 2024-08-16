c6ab96e5c0505723e9ae701c2e125f03c7444793
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-08-16 13:13:32 +0530

    Navbar done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,32 @@
 
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/shared/Navbar'
+import Login from './components/auth/Login'
+import SignUp from './components/auth/SignUp'
+import Home from './components/Home'
+
+const appRouter = createBrowserRouter([
+ {
+  path: '/',
+  element: <Home />,
+ },
+ {
+  path: '/login',
+  element: <Login />,
+ },
+ {
+  path: '/signup',
+  element: <SignUp />,
+ },
+ 
+])
 
 function App() {
-
-
   return (
     <>
-     <Navbar />
+   <RouterProvider router={appRouter} />
     </>
   )
 }
