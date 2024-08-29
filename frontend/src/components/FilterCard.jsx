192b0b8f107fb91f348a713df3f94c65916aa886
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-08-29 17:00:27 +0530

    Build filter page

---
+++ frontend/src/components/FilterCard.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { RadioGroup, RadioGroupItem } from './ui/radio-group'
+import { Label } from './ui/label'
+
+
+const filterData = [
+  {
+    filterType: 'Location',
+    array:["Delhi NCR", "Banglore", "Hyderabad", "Pune", "Mumbai"]
+  },
+  {
+    filterType: 'Industry',
+    array:["Frontend Developer", "Backend Development", "Full stack Development" ]
+  },
+  {
+    filterType: 'Salary',
+    array:["0-40K ", "42-1Lakh", "1Lakh to 5Lakh"]
+  },
+]
+
+const FilterCard = () => {
+  return (
+    <div className='w-full bg-white p-3 rounded-md'>
+        <h1> Filter Jobs</h1>
+        <hr className='mt-3'/>
+        <RadioGroup>
+          {
+            filterData.map((data, index) => (
+              <div key={index}>
+                <h1 className='font-bold text-lg'>{data.filterType}</h1>
+                {
+                  data.array.map((item, index) => {
+                   return (
+                     <div key={index} className='flex items-center space-x-2 my-2'> 
+                      <RadioGroupItem value={item}/>
+                      <Label>{item}</Label>
+                      </div>
+                   )
+                  }
+                    
+                  )
+                }
+              </div>
+            ))
+          }
+        </RadioGroup>
+    </div>
+  )
+}
+
+export default FilterCard
