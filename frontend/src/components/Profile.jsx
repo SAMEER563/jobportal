4c7f942ffb00f71a449225e948496b74b8f7ed41
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-09-10 21:11:51 +0530

    add profile section with some changes

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -44,7 +44,7 @@ const Profile = () => {
             <h1>Skills</h1>
            <div className="flex items-center gap-1">
            {
-                skills.length != 0 ? skills.map((item, index) => <Badge key={index}>{item}</Badge>) : <span> NA </span>
+                skills.length != 0 ? skills.map((item, index) => <Badge key={index}>{item}</Badge>) : <span>NA</span>
             }
            </div>
         </div>
