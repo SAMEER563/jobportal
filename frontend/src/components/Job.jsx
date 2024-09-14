55a3f60f91fcbd37d081c197dafde5cb8f4216d7
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-09-14 12:49:06 +0530

    Job description done

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -3,8 +3,13 @@ import { Button } from './ui/button'
 import { Bookmark } from 'lucide-react'
 import { Avatar, AvatarImage } from './ui/avatar'
 import { Badge } from './ui/badge'
+import { useNavigate } from 'react-router-dom'
+
+
 
 const Job = () => {
+  const navigate = useNavigate();
+  const jobId = 1;
   return (
     <div className='p-5 rounded-md shadow-lg border border-gray-100'>
       <div className='flex items-center justify-between'>
@@ -38,11 +43,11 @@ const Job = () => {
         </Badge>
       </div>
       <div className='flex items-center gap-4 mt-4'>
-        <Button variant='outline'>Details</Button>
+        <Button onClick={() => navigate(`/description/${jobId}`)} variant='outline'>Details</Button>
         <Button className='bg-blue-600'>Save For Later</Button>
       </div>
     </div>
   )
 }
 
-export default Job
+export default Job;
